80fc240f8c7e086a8f76ab2b014ba87ee0441d08
benhavinsports <jahor22@outlook.com>
2026-05-20 23:39:31 -0400

    Delete config.py

--- config.py
+++
@@ -1,1 +0,0 @@
-CANON=["game","team","opponent","pitcher","player","bats","lineup_slot","pull_pct","barrel_pct","sweet_spot_pct","hard_hit_pct","hpi","dmg","hr_pa","pitch_type","pitch_edge","hr_alert","cond_up","weak_slot_tag","laser","rakes","platoon","weak_slots","odds","public_pct","weather_score","bullpen_dmg","confirmed_lineup","dob","jersey","result_hr","notes"]
