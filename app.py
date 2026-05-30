817ac60558d3c007baa269c1e8a2a0a242722e5f
benhavinsports <jahor22@outlook.com>
2026-05-30 14:37:23 -0400

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from ui import inject_css, render_board
-
-st.set_page_config(page_title='Blender Gameboard', page_icon='🔥', layout='wide', initial_sidebar_state='collapsed')
-inject_css()
-render_board({})
